518de0c01159e8ceb214b5e24ddc1e2b94c6de32
Sabin Baniya <dummymail@tutanota.de>
2022-01-15 12:48:25 +0545

    Generics in TS

---
+++ dist/generics.js
@@ -0,0 +1,14 @@
+// const giveId = <T extends object >(obj: T) => {
+//     return {
+//         ...obj,
+//         id: Math.random().toString(16)
+//     }
+// }
+// interface UserInterface3 {
+//     name: string
+// }
+// const user4: UserInterface3 = {
+//     name: "Jhon"
+// }
+// const res = giveId<UserInterface3>(user4)
+// console.log(res)
